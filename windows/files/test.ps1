edab6961b3f1597021845691ea4fd9c1db6c8595
lidayan <dayan.li@datayes.com>
2016-03-15 17:25:54 +0800

    adfasdf

--- windows/files/test.ps1
+++ windows/files/test.ps1
@@ -1,3 +1,27 @@
 
 
 echo helloboy!
+
+Unregister-ScheduledTask -TaskName restart-computer
+
+$action = New-ScheduledTaskAction -Execute "Get-Services"
+
+$trigger = New-ScheduledTaskTrigger `
+    -Weekly `
+    -WeeksInterval 1 `
+    -DaysOfWeek Sunday `
+    -At 2am
+
+$settings = New-ScheduledTaskSettingsSet -RunOnlyIfIdle  -StartWhenAvailable
+
+$principal = New-ScheduledTaskPrincipal -LogonType "S4U" -UserId "administrator"
+
+Register-ScheduledTask `
+    -Action $action `
+    -Trigger $trigger `
+    -Settings $settings `
+    -Principal $principal `
+    -User administrator `
+    -Password ConvertTo-SecureString -String "123456" -AsPlainText -Force `
+    -Description "restart computer at saterday 2am" `
+    -TaskName "restart-task"
---
+++ windows/files/win.scheduletask.bat
@@ -0,0 +1,3 @@
+
+
+schtasks /create /ru administrator /rp 123456 /tr "shutdown /r /t 1" /sc weekly /d sat /st 2:00 
