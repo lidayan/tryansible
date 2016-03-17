61501a3a7222b636c40a52ade59af3fd63cc349f
lidayan <dayan.li@datayes.com>
2016-03-16 23:18:06 -0700

    rm dir ntp

--- windows/files/NTP/bin/startntp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM ********** START NTP SERVICE *************
-echo Trying to start NTP service:
-net start ntp
-pause
--- windows/files/NTP/bin/stopntp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM ********** STOP NTP SERVICE *************
-echo Trying to stop NTP service:
-net stop ntp
-pause
